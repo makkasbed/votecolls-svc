da313f44f49c4603cf2980a9d6258acb6d5279d8
adu <aluta182004@gmail.com>
2023-05-20 14:04:04 +0000

    implemented polling station

--- election/src/main/java/entities/PollingStation.java
+++ election/src/main/java/entities/PollingStation.java
@@ -1,11 +1,27 @@
 package entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
 @Data
 @Builder
+@NoArgsConstructor
 public class PollingStation {
+    @Id
+    @SequenceGenerator(
+            name = "polling_station_id_sequence",
+            sequenceName = "polling_station_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "polling_station_id_sequence"
+    )
     Integer id;
     String name;
     Integer constituencyId;
