7d7e3c2858018b79ba27f008190a233c546132a1
adu <aluta182004@gmail.com>
2023-05-20 14:05:46 +0000

    implemented region

--- election/src/main/java/entities/Region.java
+++ election/src/main/java/entities/Region.java
@@ -1,11 +1,27 @@
 package entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
 @Data
 @Builder
+@NoArgsConstructor
 public class Region {
+    @Id
+    @SequenceGenerator(
+            name = "region_id_sequence",
+            sequenceName = "region_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "region_id_sequence"
+    )
     Integer id;
     String name;
 }
