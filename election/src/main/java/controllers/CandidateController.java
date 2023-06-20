ddb6cacf88942a1d6165f25a98f60fee63cbea52
adu <aluta182004@gmail.com>
2023-06-20 05:43:18 +0000

    implemented candidate controller

--- election/src/main/java/controllers/CandidateController.java
+++ election/src/main/java/controllers/CandidateController.java
@@ -1,4 +1,28 @@
 package controllers;
 
+import entities.Candidate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import services.CandidateService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/candidates")
 public class CandidateController {
+    @Autowired
+    CandidateService candidateService;
+
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public Candidate submit(@RequestBody Candidate candidate){
+        return candidateService.save(candidate);
+    }
+    @GetMapping(produces = "application/json")
+    public List<Candidate> list(){
+        return candidateService.list();
+    }
+    @PutMapping(value="/{id}",produces = "application/json")
+    public Candidate update(@PathVariable Integer id,Candidate candidate){
+        return candidateService.update(id,candidate);
+    }
 }
