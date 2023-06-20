07a6942d04a807bb0a8c4a18e32f1f39795a8570
adu <aluta182004@gmail.com>
2023-06-20 05:53:03 +0000

    implemented poll detail controller

--- election/src/main/java/controllers/PollDetailController.java
+++ election/src/main/java/controllers/PollDetailController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import entities.Poll;
+import entities.PollDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import repos.PollDetailRepo;
+import services.PollDetailService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/polldetails")
 public class PollDetailController {
+    @Autowired
+    PollDetailService pollDetailService;
+
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public PollDetail submit(@RequestBody PollDetail p){
+        return pollDetailService.save(p);
+    }
+    @GetMapping(produces = "application/json",consumes = "application/json")
+    public List<PollDetail> list(){
+        return pollDetailService.list();
+    }
+    @PutMapping(value="/{id}",produces = "application/json",consumes = "application/json")
+    public PollDetail update(@PathVariable Integer id,@RequestBody PollDetail p){
+        return pollDetailService.update(id,p);
+    }
 }
